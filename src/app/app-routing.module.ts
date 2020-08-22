9888839cd45d3166e1701b922b48d58d25c278dd
Koirannaama <ollimatti.saarinen@gmail.com>
2020-08-22 18:52:06 +0300

    Add navigation between views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SummaryViewComponent } from './views/summary-view/summary-view.component';
+import { FixturesViewComponent } from './views/fixtures-view/fixtures-view.component';
 
 
 const routes: Routes = [
-    { path: '', component: SummaryViewComponent }
+    { path: '', component: SummaryViewComponent },
+    { path: 'fixtures', component: FixturesViewComponent }
 ];
 
 @NgModule({
